80732e285f6f54c567886cb9a061c6225bada82d
Adarsh Yadav <adarshyadavsy@gmail.com>
2024-11-02 22:13:26 +0530

    Formated the Files to look good

--- train_model.py
+++ train_model.py
@@ -1,22 +1,16 @@
-# Real Time
-
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
-# Set up paths to dataset
 train_dir = 'data/train'
 val_dir = 'data/validation'
-
-# Parameters
 batch_size = 500
 img_height = 48
 img_width = 48
-num_classes = 7  # Number of emotions in the dataset
+num_classes = 7
 
-# Data augmentation and data loading
 train_datagen = ImageDataGenerator(
     rescale=1./255,
     horizontal_flip=True,
@@ -43,7 +37,8 @@ val_data = val_datagen.flow_from_directory(
 )
 
 model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(img_height, img_width, 1)),
+    Conv2D(32, (3, 3), activation='relu',
+           input_shape=(img_height, img_width, 1)),
     MaxPooling2D(pool_size=(2, 2)),
     Dropout(0.25),
 
@@ -66,17 +61,17 @@ model = Sequential([
 ])
 
 model.compile(
-    optimizer=Adam(learning_rate=0.001), 
-    loss="categorical_crossentropy", 
+    optimizer=Adam(learning_rate=0.001),
+    loss="categorical_crossentropy",
     metrics=["accuracy"]
-    )
+)
 
-# Print model summary
 model.summary()
 
-# Train the model with early stopping
-early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
-lr_scheduler = ReduceLROnPlateau(monitor="val_loss", patience=3, factor=0.2, min_lr=1e-7)
+early_stopping = EarlyStopping(
+    monitor='val_loss', patience=5, restore_best_weights=True)
+lr_scheduler = ReduceLROnPlateau(
+    monitor="val_loss", patience=3, factor=0.2, min_lr=1e-7)
 
 history = model.fit(
     train_data,
@@ -85,5 +80,4 @@ history = model.fit(
     callbacks=[early_stopping, lr_scheduler]
 )
 
-# Save the model
 model.save('emotion_recognition_model_final.h5')
